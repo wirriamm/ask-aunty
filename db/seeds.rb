9ed37fe49f1d26d2dfbdfee1be7e5e695dfbe943
William Goh <wirriamm@gmail.com>
2020-06-20 12:42:31 +0800

    Seed cuisines with just names

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Destroy all cuisines
+Cuisine.destroy_all
+
+def create_cuisine(name)
+  cuis = Cuisine.new(name: name)
+  cuis.save!
+end
+
+puts "Creating cuisines"
+cuisines = %W[Western Japanese Korean Chinese Thai Vietnamese Indonesian Mexican Italian]
+cuisines.each do |cuisine|
+  create_cuisine(cuisine)
+  puts "#{Cuisine} created"
+end
