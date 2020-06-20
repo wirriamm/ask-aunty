b75da33ddebebf7e483a27c94b5a55d234982109
Jasmine Dang <ngandth.1992@gmail.com>
2020-06-20 13:08:52 +0800

    seed preferences

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Seeds preferences
+preferences = ['no spicy', 'no raw fish', 'no fast food']
+restrictions = ['halal', 'gluten free', 'kosher', 'vegan', 'vegetarian', 'allergic to seafood', 'allergic to peanuts', 'lactose free']
+seeds = preferences + restrictions
+
+puts "Seeding all preferences."
+seeds.each do |pref|
+  preference = Preference.new(name: pref)
+  if preference.save
+    puts "Seeded #{preference.name}."
+  end
+end
+puts "Seeded #{Preference.count} preferences."
