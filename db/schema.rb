ed115bb58491b7aef0b1810d16cfb65ef4b8a0fa
William Goh <wirriamm@gmail.com>
2020-06-23 23:05:03 +0800

    updated cuisines seeds limited to only 10 cuisines added dish column to cuisines table seeded images url to store in active storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_06_20_083431) do
+ActiveRecord::Schema.define(version: 2020_06_23_135957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +40,7 @@ ActiveRecord::Schema.define(version: 2020_06_20_083431) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-
+    t.string "dish"
   end
 
   create_table "meals", force: :cascade do |t|
@@ -49,7 +48,6 @@ ActiveRecord::Schema.define(version: 2020_06_20_083431) do
     t.datetime "endtime"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-
   end
 
   create_table "preferences", force: :cascade do |t|
