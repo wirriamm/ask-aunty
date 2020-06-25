9c6cf4af3cdef69a91abada8f5782ffd0ed6b817
William Goh <wirriamm@gmail.com>
2020-06-25 22:06:53 +0800

    user can join meal

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,25 +8,22 @@ class PagesController < ApplicationController
   end
 
   def create_users_meal
-    # Check if Meal ID exists
-    @meal = Meal.new
-    render :join_meal, alert: "Meal ID #{get_vanity_id} not found" if Meal.find_by(vanity_id: get_vanity_id).nil?
-
     @meal = Meal.find_by(vanity_id: get_vanity_id)
-    # Check if user have already joined this meal
-    @users_meal = UsersMeal.find_by(user: current_user, meal: @meal)
-    if @users_meal.nil?
-      # Create new users_meal
-      @users_meal = UsersMeal.new(user: current_user, meal: @meal)
-      if @users_meal.save
-        redirect_to preferences_path
-      else
-        render :join_meal, alert: "Failed to join meal #{get_vanity_id}"
-      end
+    # Check if Meal ID exists
+    if @meal.nil?
+      @meal = Meal.new
+      render :join_meal, alert: "Meal ID #{get_vanity_id} not found"
     else
-      # Contnue the poll from where it is left off
-      polls = Poll.where(user: current_user, meal: @meal)
-      redirect_to root_path # To direct to polling page or verdict page
+      # Check if user have already joined this meal
+      if @meal.users.include? current_user
+        redirect_to root_path # TO BE REMOVED
+        # redirect_to result_path
+      else
+        @meal.users << current_user
+        @meal.save
+        redirect_to root_path # TO BE REMOVED
+        # redirect_to preferences_path
+      end
     end
   end
 
