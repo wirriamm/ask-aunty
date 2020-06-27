08719a2601637a0eb0e5140a00fb4a94ac3b61e6
Jasmine Dang <ngandth.1992@gmail.com>
2020-06-27 12:40:34 +0800

    changed background color

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -34,15 +34,14 @@ class MealsController < ApplicationController
     @meal = Meal.find(params[:id])
     @endtime = @meal.endtime
     @polls =  Poll.where(meal_id: @meal.id)
-              .select("cuisine_id, score")
-              .group("cuisine_id")
-              .order("cuisine_id").sum("score")
+                  .select("cuisine_id, score")
+                  .group("cuisine_id")
+                  .order("cuisine_id").sum("score")
     # @poll_summary = @polls.select("cuisine_id, score").group("cuisine_id").sum("score").order("score")
     @polls_sorted = @polls.sort_by { |cuisine, score| score }
     @first = Cuisine.find(@polls_sorted.reverse.first[0])
     @second = Cuisine.find(@polls_sorted.reverse.second[0])
     @third = Cuisine.find(@polls_sorted.reverse.third[0])
-
 
     # @poll_summary.order(:value).reverse_order
     # @poll_summary = {}
