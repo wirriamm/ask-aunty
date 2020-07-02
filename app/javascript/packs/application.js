d5196269f455214f336b3014a7222a2441d22137
William Goh <wirriamm@gmail.com>
2020-07-02 19:53:27 +0800

    bootstrap tooltip fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -33,6 +33,7 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   loadDynamicBannerText();
+  $('[data-toggle="tooltip"]').tooltip();
 });
 
 import "controllers"
