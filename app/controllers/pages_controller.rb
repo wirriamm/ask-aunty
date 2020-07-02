865f990a97516682362ac3d5b6993e8f69d7afbe
William Goh <wirriamm@gmail.com>
2020-07-03 02:46:08 +0800

    hide form label when needed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,27 +1,12 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
-  # def landing
-  # end
-
   def home
     @meal = Meal.new
   end
 
   def join_meal
     @meal = Meal.new
-  end
-
-  def join_meal_ajax
-    @meal = Meal.new
-    # respond_to do |format|
-    #   format.html
-    #   format.json {
-    #     render json: {
-    #       meal: @meal
-    #     }
-    #   }
-    # end
   end
 
   def create_users_meal
