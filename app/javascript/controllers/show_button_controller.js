171579fbdce380161e1b957bda2e621af7a7dd93
Jasmine Dang <ngandth.1992@gmail.com>
2020-07-05 15:32:11 +0800

    Start btn is hidden if endtime is over

--- app/javascript/controllers/show_button_controller.js
+++ app/javascript/controllers/show_button_controller.js
@@ -1,19 +1,25 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = [ 'startPoll', 'viewResult', 'pollCount' ];
+  static targets = [ 'startPoll', 'viewResult', 'pollCount', 'time' ];
 
   connect() {
     console.log('Hello!');
     const startBtn = this.startPollTarget;
     const resultBtn = this.viewResultTarget;
     const pollCount = this.pollCountTarget.value;
-    if (pollCount >= 10) {
-      startBtn.style.display = 'none';
+    const time = this.timeTarget.value === 'true';
+
+    if (time) {
+      if (pollCount >= 10) {
+        startBtn.style.display = 'none';
+      } else {
+        resultBtn.style.display = 'none';
+        if (pollCount > 0)
+          startBtn.innerText = "Continue the poll";
+      }
     } else {
-      resultBtn.style.display = 'none';
-      if (pollCount > 0)
-        startBtn.innerText = "Continue the poll";
+        startBtn.style.display = 'none';
     }
   }
 }
